413d14b6349e703bcf09b3b176ddd8b70f0ff1b5
Marc Wiedermann <marcwiedermann@posteo.de>
2023-03-02 12:01:05 +0100

    update notebook names

--- scripts/execute_notebooks.sh
+++ scripts/execute_notebooks.sh
@@ -1,12 +1,12 @@
 to_execute=(
-    '1.1-plot_example_timeseries.ipynb'
-    '1.2-plot_average_trajectories.ipynb'
-    '1.3-plot_distributions_of_vital_changes.ipynb'
-    '1.4-plot_frequencies_of_extreme_vitals.ipynb'
-    '1.5-table_with_basic_user_demographics.ipynb'
-    '1.6-plot_positive_pcr_tests_per_month.ipynb'
-    '1.7-plot_time_between_vaccination_and_infection.ipynb'
-    '1.8-plot_comparison_age_groups_datenspende_and_overall_population.ipynb'
+    '1.01-plot_example_timeseries.ipynb'
+    '1.02-plot_average_trajectories.ipynb'
+    '1.03-plot_distributions_of_vital_changes.ipynb'
+    '1.04-plot_frequencies_of_extreme_vitals.ipynb'
+    '1.05-table_with_basic_user_demographics.ipynb'
+    '1.06-plot_positive_pcr_tests_per_month.ipynb'
+    '1.07-plot_time_between_vaccination_and_infection.ipynb'
+    '1.08-plot_comparison_age_groups_datenspende_and_overall_population.ipynb'
 )
 
 cd notebooks
